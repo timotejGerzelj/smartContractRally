8181cc8941a8339713f0cfe1aaa14c1953296fbb
timotejGerzelj <timotej.gerzelj@gmail.com>
2023-01-06 14:08:13 +0100

    Put some files out of gitignore

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle"); //new update from hardhat requires to import 'hardhat-toolbox'
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require('@openzeppelin/hardhat-upgrades');
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+      allowUnlimitedContractSize: true
+    },
+  },
+};
